e3b765f4762f67d070faadae0f954a7bda33e1ac
Jazmin Natalia Cordoba <jn.cordobap1@uniandes.edu.co>
2024-11-30 19:04:23 -0500

    mailgun

---
+++ cypress/e2e/settings_management/E039-modify_mailgun_dominio.cy.js
@@ -0,0 +1,44 @@
+import { faker } from '@faker-js/faker';
+
+describe('Configure Custom Integration description', () => {
+    const LOCAL_HOST = Cypress.env('LOCAL_HOST');
+    const SCREENSHOT_PATH = 'E018-configure_custom_integration_description_before/configure_custom_integration';
+    let screenshotCounter = 1;
+
+    function takeScreenshot() {
+        cy.screenshot(`${SCREENSHOT_PATH}_${screenshotCounter}`);
+        screenshotCounter++;
+    }
+
+    beforeEach("Precondition: Admin login", () => {
+        cy.LoginGhost();
+    });
+
+    it('Configure a custom integration in the advanced section and verify its creation', () => {
+        cy.visit(LOCAL_HOST + "#/settings");
+
+        cy.get(
+            '#admin-x-settings-scroller > div > div:nth-child(5) > div > div:nth-child(4) > div.flex.items-start.justify-between.gap-4 > div:nth-child(2) > div > button'
+        ).click();
+
+        const domain = faker.internet.domainName();
+
+        cy.get('input.peer')
+        .first() 
+        .should('be.visible') 
+        .clear() 
+        .type(domain) 
+
+        cy.get('body').click();
+
+        cy.contains('button', 'Save')
+            .click();
+
+        cy.get(
+            '#admin-x-settings-scroller > div > div:nth-child(5) > div > div:nth-child(4) > div.flex.items-start.justify-between.gap-4 > div:nth-child(2) > div > button'
+        ).click();
+
+        cy.get('input.peer').first() 
+            .should('have.value', domain);
+    });
+});
